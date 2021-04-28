8a0ab708e8b30fc8e6c0a12ed9d0a6186a22a9d3
phil cowan <philipjcowan1@gmail.com>
2021-04-27 17:22:05 -0700

    adding a comment to demonstrate a merge

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
-
+//add pizzas prefix to pizzaRoutes
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
